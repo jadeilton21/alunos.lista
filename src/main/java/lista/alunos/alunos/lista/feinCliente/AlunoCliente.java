83e3118a70883c1f268ca212f5e2490a863b9bc7
Jadeilton <jadeiltondm@gmail.com>
2024-03-07 16:38:11 -0300

    feat: add method get

--- src/main/java/lista/alunos/alunos/lista/feinCliente/AlunoCliente.java
+++ src/main/java/lista/alunos/alunos/lista/feinCliente/AlunoCliente.java
@@ -1,4 +1,17 @@
 package lista.alunos.alunos.lista.feinCliente;
 
+import lista.alunos.alunos.lista.doMain.Aluno;
+import lista.alunos.alunos.lista.reponse.AlunoReponseList;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "aluno-api", url = "")
 public interface AlunoCliente {
+
+
+    @GetMapping
+    AlunoReponseList getAluno();
 }
