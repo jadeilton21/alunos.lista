ca36f6a6434b33a89fd2ab387affa350880fc9c9
Jadeilton <jadeiltondm@gmail.com>
2024-03-07 16:14:35 -0300

    Initial commit

---
+++ src/main/java/lista/alunos/alunos/lista/Application.java
@@ -0,0 +1,13 @@
+package lista.alunos.alunos.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
