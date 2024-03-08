ee8c59ca6589ebbb3930b20d8a4e61c723d1a061
Jadeilton <jadeiltondm@gmail.com>
2024-03-08 15:17:32 -0300

    feat: create arguments in AlunoReponseList

--- src/main/java/lista/alunos/alunos/lista/reponse/AlunoReponseList.java
+++ src/main/java/lista/alunos/alunos/lista/reponse/AlunoReponseList.java
@@ -1,4 +1,16 @@
 package lista.alunos.alunos.lista.reponse;
 
+import lista.alunos.alunos.lista.doMain.Aluno;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AlunoReponseList {
+
+    private List<Aluno> alunosList;
 }
