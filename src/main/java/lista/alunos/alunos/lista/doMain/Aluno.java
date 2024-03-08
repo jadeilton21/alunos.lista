092cc495e261f24d5c094182da96f3a21e829eb7
Jadeilton <jadeiltondm@gmail.com>
2024-03-08 09:54:36 -0300

    feat: create class Aluno argumenths

--- src/main/java/lista/alunos/alunos/lista/doMain/Aluno.java
+++ src/main/java/lista/alunos/alunos/lista/doMain/Aluno.java
@@ -1,4 +1,22 @@
 package lista.alunos.alunos.lista.doMain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aluno {
+
+
+
+    private String nome;
+    private String cpf;
+    private String cidade;
+    private String rua;
+    private String bairro;
+    private String nomeMae;
+    private String nomePai;
+
+
+
 }
