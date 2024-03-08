128f270960e6e631896edfe901acc70d7b95ef10
Jadeilton <jadeiltondm@gmail.com>
2024-03-08 09:59:50 -0300

    feat: create AlunoService arguments

--- src/main/java/lista/alunos/alunos/lista/service/AlunoService.java
+++ src/main/java/lista/alunos/alunos/lista/service/AlunoService.java
@@ -1,15 +1,26 @@
 package lista.alunos.alunos.lista.service;
 
+import lista.alunos.alunos.lista.doMain.Aluno;
+import lista.alunos.alunos.lista.feinCliente.AlunoCliente;
+import lista.alunos.alunos.lista.reponse.AlunoReponseList;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AlunoService {
 
 
-    private final AlunoService alunoService;
+    private final AlunoCliente alunoCliente;
 
 
     public AlunoService(AlunoService alunoService) {
         this.alunoService = alunoService;
+    }
+
+
+    public List<Aluno> getAlunosFromLibriary(){
+        AlunoReponseList reponseList = this.alunoCliente.getAluno();
+        return reponseList.getAlunosList();
     }
 }
