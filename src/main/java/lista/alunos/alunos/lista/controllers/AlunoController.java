a5faa7d24cc45087887297b6881eede2ad89359c
Jadeilton <jadeiltondm@gmail.com>
2024-03-08 09:43:59 -0300

    feat: create methods in AlunoController

--- src/main/java/lista/alunos/alunos/lista/controllers/AlunoController.java
+++ src/main/java/lista/alunos/alunos/lista/controllers/AlunoController.java
@@ -1,4 +1,29 @@
 package lista.alunos.alunos.lista.controllers;
 
+import lista.alunos.alunos.lista.doMain.Aluno;
+import lista.alunos.alunos.lista.service.AlunoService;
+import org.apache.catalina.LifecycleState;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/aluno")
 public class AlunoController {
+
+
+    private final AlunoService alunoService;
+
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+
+
+    public ResponseEntity<List<Aluno>> getAlunos(){
+        List<Aluno> alunos = this.alunoService.getAlunosFromLibriary();
+    }
 }
